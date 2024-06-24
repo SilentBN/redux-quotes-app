9833f671efb7283bc553d1ae80483aab05374c01
SilentBN <61912362+SilentBN@users.noreply.github.com>
2024-06-23 21:50:09 -0400

    chore: Add Redux Provider and store to index.js for managing state

--- frontend/index.js
+++ frontend/index.js
@@ -1,13 +1,17 @@
-import './styles/reset.css'
-import './styles/styles.css'
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './components/App'
+import "./styles/reset.css";
+import "./styles/styles.css";
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux"; // ✨ import the Provider
+import store from "./state/store"; // ✨ import the store
+import App from "./components/App";
 
-const domNode = document.getElementById('root')
-const root = createRoot(domNode)
+const domNode = document.getElementById("root");
+const root = createRoot(domNode);
 
-// ✨ wrap <App /> with a provider
+// ✨ wrap <App /> with a provider that uses the store as a prop
 root.render(
-  <App />
-)
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
