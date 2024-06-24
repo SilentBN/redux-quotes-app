e35df31b73f1a41762ff5c508c2b88143a60fdd6
SilentBN <61912362+SilentBN@users.noreply.github.com>
2024-06-23 22:58:24 -0400

    Quotes.js Explain

--- frontend/components/Quotes.js
+++ frontend/components/Quotes.js
@@ -7,8 +7,12 @@ import {
   editQuoteAuthenticity,
 } from "../state/quotesSlice";
 
+// We import useSelector and useDispatch hooks from React Redux. These are the primary ways a component interacts with Redux.
+// We also import the action creators from our quotesSlice.
+
 export default function Quotes() {
-  const dispatch = useDispatch();
+  const dispatch = useDispatch(); // This hook gives us a reference to the Redux store's dispatch function. We use this to send actions to the store.
+  // Using useSelector:
   const quotes = useSelector((state) => state.quotes.quotes);
   const displayAllQuotes = useSelector(
     (state) => state.quotes.displayAllQuotes
@@ -16,6 +20,8 @@ export default function Quotes() {
   const highlightedQuote = useSelector(
     (state) => state.quotes.highlightedQuote
   );
+  // useSelector allows us to extract data from the Redux store state.
+  // Each call to useSelector returns a specific piece of the state.
 
   return (
     <div id="quotes">
@@ -32,6 +38,8 @@ export default function Quotes() {
                 highlightedQuote === qt.id ? " highlight" : ""
               }`}
             >
+              {/* We filter the quotes based on displayAllQuotes state.
+                Then we map over the filtered quotes to render each one. */}
               <div>{qt.quoteText}</div>
               <div>{qt.authorName}</div>
               <div className="quote-buttons">
@@ -43,6 +51,7 @@ export default function Quotes() {
                 >
                   DELETE
                 </button>
+                {/* dispatch(deleteQuote(qt.id)) sends an action to delete a specific quote. */}
                 <button
                   onClick={() =>
                     /* ✨ dispatch an action */
@@ -51,6 +60,7 @@ export default function Quotes() {
                 >
                   HIGHLIGHT
                 </button>
+                {/* dispatch(setHighlightedQuote(qt.id)) highlights a quote. */}
                 <button
                   onClick={() =>
                     /* ✨ dispatch an action */
@@ -59,6 +69,7 @@ export default function Quotes() {
                 >
                   FAKE
                 </button>
+                {/* dispatch(editQuoteAuthenticity(qt.id)) toggles whether a quote is considered apocryphal. */}
               </div>
             </div>
           ))}
@@ -74,6 +85,10 @@ export default function Quotes() {
           {displayAllQuotes ? "HIDE" : "SHOW"} FAKE QUOTES
         </button>
       )}
+      {/* This button dispatches the toggleVisibility action to show/hide apocryphal quotes. */}
     </div>
   );
 }
+
+// This component demonstrates how to read state from Redux (using useSelector) and how to update state (using dispatch). The component doesn't manage any state itself; it relies entirely on Redux for its data and actions.
+// By using Redux in this way, we've separated our state management from our UI components. This can make our application easier to understand and maintain, especially as it grows in complexity.
