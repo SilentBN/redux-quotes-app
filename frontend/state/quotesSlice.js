392e4ef505447c417e5b5da1913c807392382f70
SilentBN <61912362+SilentBN@users.noreply.github.com>
2024-06-23 22:41:40 -0400

    Explaining code for quotesSlice.js

--- frontend/state/quotesSlice.js
+++ frontend/state/quotesSlice.js
@@ -1,7 +1,10 @@
 import { createSlice } from "@reduxjs/toolkit";
+// We import createSlice from Redux Toolkit. This function helps us create a slice of Redux state along with its actions and reducers.
 
 let id = 4; // Start from 4 since we already have 3 initial quotes
 const getNextId = () => id++;
+
+// We set up a simple ID generator. This is outside the Redux state because we don't need React to re-render when this changes.
 
 const initialState = {
   displayAllQuotes: true,
@@ -28,13 +31,23 @@ const initialState = {
   ],
 };
 
+// We define our initial state. This is what our Redux store will look like when the app starts. It includes:
+// displayAllQuotes: A boolean to toggle between showing all quotes or only non-apocryphal ones
+// highlightedQuote: The ID of the currently highlighted quote (if any).
+// quotes: An array of quote objects.
+
 const quotesSlice = createSlice({
   name: "quotes",
   initialState,
+  // We create our slice using createSlice. This function takes an object with:
+  // name: A string name for this slice of state.
+  // initialState: The initial state we defined earlier.
+  //reducers: An object where each key is an action name, and each value is a reducer function.
   reducers: {
     toggleVisibility: (state) => {
       state.displayAllQuotes = !state.displayAllQuotes;
     },
+    // toggleVisibility: This reducer toggles the displayAllQuotes boolean. With Redux Toolkit, we can mutate the state directly thanks to Immer library working behind the scenes.
     deleteQuote: (state, action) => {
       state.quotes = state.quotes.filter(
         (quote) => quote.id !== action.payload
@@ -43,15 +56,18 @@ const quotesSlice = createSlice({
         state.highlightedQuote = null;
       }
     },
+    // deleteQuote: This reducer removes a quote from the array based on the ID passed in action.payload. It also clears the highlighted quote if the deleted quote was highlighted.
     editQuoteAuthenticity: (state, action) => {
       const quote = state.quotes.find((quote) => quote.id === action.payload);
       if (quote) {
         quote.apocryphal = !quote.apocryphal;
       }
     },
+    // editQuoteAuthenticity: This reducer toggles the apocryphal property of a quote based on the ID passed in action.payload.
     setHighlightedQuote: (state, action) => {
       state.highlightedQuote = action.payload;
     },
+    // setHighlightedQuote: This reducer sets the highlightedQuote to the ID passed in action.payload.
     createQuote: (state, action) => {
       const newQuote = {
         id: getNextId(),
@@ -61,6 +77,7 @@ const quotesSlice = createSlice({
       };
       state.quotes.push(newQuote);
     },
+    // createQuote: This reducer creates a new quote object and adds it to the quotes array. The quote details are passed in action.payload.
   },
 });
 
@@ -73,3 +90,5 @@ export const {
 } = quotesSlice.actions;
 
 export default quotesSlice.reducer;
+
+// We export the action creators and the reducer. Redux Toolkit automatically creates action creators with the same names as our reducers.
