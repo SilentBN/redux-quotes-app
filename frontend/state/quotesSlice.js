d27009510d0b52b3d7ce42b14f3dba0d4d91bcc2
SilentBN <61912362+SilentBN@users.noreply.github.com>
2024-06-23 21:40:23 -0400

    chore: Update quotesSlice.js to use Redux Toolkit

--- frontend/state/quotesSlice.js
+++ frontend/state/quotesSlice.js
@@ -1,28 +1,29 @@
-// ✨ create your `quotesSlice` in this module
+import { createSlice } from "@reduxjs/toolkit";
 
-let id = 1
-const getNextId = () => id++
+let id = 4; // Start from 4 since we already have 3 initial quotes
+const getNextId = () => id++;
+
 const initialState = {
   displayAllQuotes: true,
   highlightedQuote: null,
   quotes: [
     {
-      id: getNextId(),
+      id: 1,
       quoteText: "Don't cry because it's over, smile because it happened.",
       authorName: "Dr. Seuss",
       apocryphal: false,
     },
     {
-      id: getNextId(),
+      id: 2,
       quoteText: "So many books, so little time.",
       authorName: "Frank Zappa",
       apocryphal: false,
     },
     {
-      id: getNextId(),
+      id: 3,
       quoteText: "Be yourself; everyone else is already taken.",
       authorName: "Oscar Wilde",
       apocryphal: false,
     },
   ],
-}
+};
